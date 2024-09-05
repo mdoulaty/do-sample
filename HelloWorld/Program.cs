d315b7364ad0689d02fe603d58b1af01e2125f37
Morrie Doulaty <mdoulaty@fb.com>
2024-09-05 07:24:51 +0100

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!!! How are you?");
 
 app.Run("http://0.0.0.0:8080");
